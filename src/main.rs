411dc8393d0ae597b40f8a7318b0d748632a7489
yuma140902 <yuma140902@gmail.com>
2022-02-06 11:36:59 +0900

    Feat: CLIの定義

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(name = "dotfiles-rs", author, version, about = "dotfiles organizer")]
+struct AppArg {
+    #[clap(subcommand)]
+    subcommand: AppSubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum AppSubCommand {
+    #[clap(visible_alias = "p")]
+    /// Pick files or directories to manage dotfile repository
+    Pick {
+        #[clap(
+            long = "repo",
+            short,
+            default_value = "./repos/dotfiles",
+            env = "DOTFILES_REPO"
+        )]
+        /// dotfilesリポジトリの場所。相対パスの場合はホームディレクトリを基準とする。
+        repository: PathBuf,
+
+        files_and_dirs: Vec<PathBuf>,
+    },
+    #[clap(visible_alias = "i")]
+    /// Install dotfiles to system
+    Install {
+        #[clap(
+            long = "repo",
+            short,
+            default_value = "./repos/dotfiles",
+            env = "DOTFILES_REPO"
+        )]
+        /// dotfilesリポジトリの場所。相対パスの場合はホームディレクトリを基準とする。
+        repository: PathBuf,
+    },
+}
 fn main() {
-    println!("Hello, world!");
+    let arg = AppArg::parse();
+    dbg!(&arg);
 }
